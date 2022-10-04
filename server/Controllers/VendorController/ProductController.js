a4135baa67f6993613b008b61c0968da18f3e205
Mohammad Ammad <mohammadammad144@gmail.com>
2022-10-04 16:48:14 +0500

    product api

--- server/Controllers/VendorController/ProductController.js
+++ server/Controllers/VendorController/ProductController.js
@@ -1,7 +1,8 @@
 //---IMPORTS
 const Attributes = require('../../models/Attributes')
 const Products = require('../../models/Products')
-
+const Sizes = require('../../models/Sizes')
+const Color = require('../../models/Color')
 
 //---CREATE PRODUCT
 exports.create = async (req, res) => {
@@ -16,19 +17,19 @@ exports.create = async (req, res) => {
             await Promise.all(
                 attributes.map(async item => {
                     const attr = new Attributes({
-                        image:item.image,
-                        sizeId:item.sizeId,
-                        colorId:item.colorId,
-                        availabilty:item.availabilty,
-                        native_price:item.native_price,
-                        crypto_price:item.crypto_price
+                        image: item.image,
+                        sizeId: item.sizeId,
+                        colorId: item.colorId,
+                        availabilty: item.availabilty,
+                        native_price: item.native_price,
+                        crypto_price: item.crypto_price
                     })
-        
+
                     const _attr = await attr.save();
                     arr.push(_attr._id);
                 })
             )
-          
+
             const resp = new Products({
                 vendorAddress,
                 catId,
@@ -55,31 +56,118 @@ exports.list = async (req, res) => {
         await Promise.all(
             products.map(async item => {
                 await Promise.all(
-                        item.attribute.map(async el => {
-                        const attr = await Attributes.findById({_id:el})
+                    item.attribute.map(async el => {
+                        const attr = await Attributes.findById({ _id: el })
+                        const _size = await Sizes.findById({ _id: attr.sizeId })
+                        const _clr = await Color.findById({ _id: attr.colorId })
                         let _obj = {
-                            image:attr.image,
-                            sizeId:attr.sizeId,
-                            colorId:attr.colorId,
-                            availabilty:attr.availabilty,
-                            native_price:attr.native_price,
-                            crypto_price:attr.crypto_price
+                            image: attr.image,
+                            size: _size.title,
+                            color: _clr.color,
+                            availabilty: attr.availabilty,
+                            native_price: attr.native_price,
+                            crypto_price: attr.crypto_price
                         }
                         attr_array.push(_obj)
                     })
                 )
                 let obj = {
-                    _id:item._id,
-                    vendorAddress:item.vendorAddress,
-                    title:item.title,
-                    description:item.description,
-                    attribute:attr_array
+                    _id: item._id,
+                    vendorAddress: item.vendorAddress,
+                    title: item.title,
+                    description: item.description,
+                    attribute: attr_array
                 }
 
                 arr.push(obj)
+                attr_array = [];
             })
         )
         res.status(200).json(arr)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+//---PRODUCT BY VENDOR ADDRESS
+exports.listByVendorAddress = async (req, res) => {
+    try {
+        const products = await Products.find({ vendorAddress: req.params.address })
+        let arr = [];
+        let attr_array = [];
+        await Promise.all(
+            products.map(async item => {
+                await Promise.all(
+                    item.attribute.map(async el => {
+                        const attr = await Attributes.findById({ _id: el })
+                        const _size = await Sizes.findById({ _id: attr.sizeId })
+                        const _clr = await Color.findById({ _id: attr.colorId })
+                        let _obj = {
+                            image: attr.image,
+                            size: _size.title,
+                            color: _clr.color,
+                            availabilty: attr.availabilty,
+                            native_price: attr.native_price,
+                            crypto_price: attr.crypto_price
+                        }
+                        attr_array.push(_obj)
+                    })
+                )
+                let obj = {
+                    _id: item._id,
+                    vendorAddress: item.vendorAddress,
+                    title: item.title,
+                    description: item.description,
+                    attribute: attr_array
+                }
+
+                arr.push(obj)
+                attr_array = [];
+            })
+        )
+        res.status(200).json(arr)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+//---PRODUCT BY ID
+exports.listById = async (req, res) => {
+    try {
+        const product = await Products.findById({ _id: req.params.id })
+        let arr = [];
+        let attr_array = [];
+
+        await Promise.all(
+            product.attribute.map(async el => {
+                const attr = await Attributes.findById({ _id: el })
+                const _size = await Sizes.findById({ _id: attr.sizeId })
+                const _clr = await Color.findById({ _id: attr.colorId })
+                let _obj = {
+                    image: attr.image,
+                    size: _size.title,
+                    color: _clr.color,
+                    availabilty: attr.availabilty,
+                    native_price: attr.native_price,
+                    crypto_price: attr.crypto_price
+                }
+                attr_array.push(_obj)
+            })
+
+        )
+
+        let obj = {
+            _id: product._id,
+            vendorAddress: product.vendorAddress,
+            title: product.title,
+            description: product.description,
+            attribute: attr_array
+        }
+
+        arr.push(obj)
+
+        res.status(200).json(arr)
+
     } catch (error) {
         res.status(500).json(error.message)
     }
--- server/Routes/ProductRoute.js
+++ server/Routes/ProductRoute.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {create, list} = require("../Controllers/VendorController/ProductController")
+const {create, list, listByVendorAddress, listById} = require("../Controllers/VendorController/ProductController")
 
 //---ROUTES
 router.route("/create").post(create);
 router.route("/").get(list);
+router.route("/vendor/:address").get(listByVendorAddress);
+router.route("/:id").get(listById);
 
 
 module.exports = router;
